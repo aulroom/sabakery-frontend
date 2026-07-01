17a8ffec4521cc4062687bf5223ed206159eacb6
Rifqy Aditya Elfateh <kukukunaon01@gmail.com>
2026-07-01 22:58:50 +0700

    Fix import API di Kasir

--- src/pages/CashierDashboard.jsx
+++ src/pages/CashierDashboard.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import API from '../services/api'; // Pastikan path ini mengarah ke file api.js 
 
 function CashierDashboard() {
   const navigate = useNavigate();
